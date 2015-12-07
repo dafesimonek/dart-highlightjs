aae27c2465caf4400cca9dfcf0caae2bb5772f3e
dafe <dafesimonek@gmail.com>
2015-12-07 10:56:48 +0100

    initial version

---
+++ web/main.dart
@@ -0,0 +1,50 @@
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'dart:html';
+import 'package:js/js.dart';
+
+/** Direct mapping to our own js function from test.js */
+@JS("jsAlert")
+external jsAlert(String msg);
+
+/** Use dart getter for accessing published js objects,
+ * hljs object from highlightjs lib in this case
+ */
+@JS("hljs")
+external Hljs get hljs;
+
+/** Mapping to highlightjs API, see http://highlightjs.readthedocs.org/en/latest/api.html
+ * Must be marked as anonymous, as js methods are exposed using hljs object literal.
+ * Note that subset of js API can be mapped.
+ */
+@JS()
+@anonymous
+class Hljs {
+
+  /** Note that external keyword must be used and signatutes must match, try it! */
+
+  external void initHighlighting();
+
+  external List<String> listLanguages();
+
+}
+
+
+void main() {
+
+  var highlighter = querySelector('#highlight') as ButtonElement;
+  var showLanguages = querySelector('#showLanguages') as ButtonElement;
+
+  // highlight by calling into jshl library
+  highlighter.onClick.listen((evt) {
+    hljs.initHighlighting();
+  });
+
+  // show jshl supported languages, combination of js interop calls
+  showLanguages.onClick.listen((evt) {
+    //jsAlert('Default supported languages: ${hljs.listLanguages().join(", ")}');
+    window.alert('Default supported languages: ${hljs.listLanguages().join(", ")}');
+  });
+
+}
